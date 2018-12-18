444bda1d71c5ec32484543f5957c7d974be9726e
StanislavBerezin <stanislav_berezin@hotmail.com>
2018-12-18 16:46:11 +1000

    Loops

---
+++ Basics/Basics/Loops.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    class Loops
+    {
+        //ALL LOOPS POSITIONED HERE
+
+        //WHILE LOOPS
+        public void whileLoop()
+        {
+            int number = 0;
+
+            while (number < 5)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+        }
+        //breaks the while loop
+        public void whileBreakLoop()
+        {
+            int i = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Value of i: {0}", i);
+
+                i++;
+
+                if (i > 10)
+                    break;
+            }
+        }
+
+        //FOR LOOP
+        public void forLoop()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Value of i: {0}", i);
+            }
+        }
+
+        //FOR LOOP until certain condition is true
+        public void forConditionLoop()
+        {
+            int i = 0;
+
+            for (; ; )
+            {
+                //can be used infinitely
+                if (i < 10)
+                {
+                    Console.WriteLine("Value of i: {0}", i);
+                    i++;
+                }
+                else
+                    break;
+            }
+        }
+      
+
+        // DO WHILE, executes the code at least once to see the conditions
+
+}
+}
---
+++ Basics/Basics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    // Classess are like a container of code needed for your app
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Loops miniLoops = new Loops();
+
+            miniLoops.forConditionLoop();
+
+            stopApp();          
+        }
+
+        static void stopApp()
+        {
+
+            Console.WriteLine("Ready to quit? hit Enter");
+            Console.ReadLine();
+
+            
+           
+        }
+    }
+}
