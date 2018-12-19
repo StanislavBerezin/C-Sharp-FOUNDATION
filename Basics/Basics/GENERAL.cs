0104a9d1c704f38d2cfa9ce6b35a939ad92dfbbd
StanislavBerezin <stanislav_berezin@hotmail.com>
2018-12-19 18:48:25 +1000

    finished 19/12/2018

--- Basics/Basics/GENERAL.cs
+++ Basics/Basics/GENERAL.cs
@@ -8,5 +8,36 @@ namespace Basics
 {
     class GENERAL
     {
+        /*
+         * This file just to throw some basic understanding, will be used in readme on github
+         * 
+         * STATIC => non-instantable, if it applied to a class no "new" keyword can be used
+         * if static applied to a class method, property, variable, then it can be utilised 
+         * without creating an object itself. There is a case when static constructor
+         * could be used but it will run only once when the first object is created.
+         * 
+         * 
+         * ABSTRACT CLASS => used as a base class, lets say we have an ANIMAL base class
+         * we dont know what animal its going to be, that would have to be defined in DOG class
+         * which will create its own functionality based on ANIMAL Abstract class. Basically its a requirement
+         * of how a class should look like and what methods it needs to have. Can have no method
+         * declaration or can have some of it unlike interface.
+         * 
+         * 
+         * 
+         * 
+         * INTERFACE - has methods but not defined
+         * 
+         * 
+         * 
+         * OTHER
+         * 
+         * private -> can be acessed in the same CS file, hence no external can acess it
+         * public -> all can acess it
+         * protected -> has to inherit to access it
+         * 
+         * 
+         * Collections: to hold values or objects
+         * */
     }
 }
