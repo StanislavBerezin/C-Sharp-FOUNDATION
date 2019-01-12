ae311edc7b8a1e9c858400433ff80f91d1037e5b
StanislavBerezin <stanislav_berezin@hotmail.com>
2019-01-12 17:25:01 +1000

    Generics and file writing

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,66 @@
+﻿using ConsoleUI.Models;
+using ConsoleUI.WithGenerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemonstrateTextFileStorage();
+            Console.WriteLine();
+            Console.Write("Press a button to shutdown");
+            Console.ReadLine();
+        }
+
+
+        private static void DemonstrateTextFileStorage()
+        {
+            List<Person> people = new List<Person>();
+            List<LogEntry> logs = new List<LogEntry>();
+
+
+            //create a folder 'data' in your C hard drive
+            string peopleFile = @"C:\data\people.csv";
+            string logFile = @"C:\data\logs.csv";
+
+            PopulateLists(people, logs);
+
+            /* New way of doing things - generics */
+            GenericTextFileProcessor.SaveToTextFile<Person>(people, peopleFile);
+            GenericTextFileProcessor.SaveToTextFile<LogEntry>(logs, logFile);
+
+            var newPeople = GenericTextFileProcessor.LoadFromTextFile<Person>(peopleFile);
+
+            foreach (var p in newPeople)
+            {
+                Console.WriteLine($"{ p.FirstName } { p.LastName } (IsAlive = { p.IsAlive })");
+            }
+
+            var newLogs = GenericTextFileProcessor.LoadFromTextFile<LogEntry>(logFile);
+
+            foreach (var log in newLogs)
+            {
+                Console.WriteLine($"{ log.ErrorCode }: { log.Message } at { log.TimeOfEvent.ToShortTimeString() }");
+            }
+
+       
+        }
+
+        private static void PopulateLists(List<Person> people, List<LogEntry> logs)
+        {
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey" });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm", IsAlive = false });
+            people.Add(new Person { FirstName = "Greg", LastName = "Olsen" });
+
+            logs.Add(new LogEntry { Message = "I blew up", ErrorCode = 9999 });
+            logs.Add(new LogEntry { Message = "I'm too awesome", ErrorCode = 1337 });
+            logs.Add(new LogEntry { Message = "I was tired", ErrorCode = 2222 });
+        }
+    }
+}
